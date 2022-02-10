66e745c9b24c4c3901c2990f1e90f8b1141cb735
celesarthana <93391769+celesarthana@users.noreply.github.com>
2022-02-10 14:13:59 +0530

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("\n\n\n\n Hello, World! \n\n\n\n\n");
+   printf("\n\n\n\n\n Hello, World! \n\n\n\n\n");
    return 0;
 }
